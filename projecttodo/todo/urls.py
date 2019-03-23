1eeafe0558f62ce7ce90834304ecb75e4863a6b2
Mwangik <gkmwangi0@gmail.com>
2019-03-23 03:10:44 +0300

    add,delete completed and all todo list items

--- projecttodo/todo/urls.py
+++ projecttodo/todo/urls.py
@@ -4,4 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('add', views.addTodo, name='add'),
+    path('complete/<todo_id>', views.completeTodo, name='complete'),
+    path('del/completed', views.delete_completed, name='delete_completed'),
+    path('del/all', views.delete_all, name='delete_all')
 ]
--- projecttodo/todo/views.py
+++ projecttodo/todo/views.py
@@ -1,12 +1,48 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.http import require_POST
 
 from .models import Todo
+from .forms import TodoForm
 
 # Create your views here.
 def index(request):
     todo_list = Todo.objects.order_by('-id')
 
+    form = TodoForm()
+
     context={
-        'todo_list': todo_list
+        'todo_list': todo_list,
+        'form': form
     }
     return render(request, 'todo/index.html',context)
+
+@require_POST
+def addTodo(request):
+    form = TodoForm(request.POST)
+
+    if form.is_valid():
+        new_todo = Todo(text=form.cleaned_data['text'])
+        new_todo.save()
+
+    return redirect('index')
+
+
+def completeTodo(request, todo_id):
+    todo = Todo.objects.get(pk=todo_id)
+    todo.complete = True
+    todo.save()
+
+    return redirect('index')
+
+def delete_completed(request):
+    completed = Todo.objects.filter(complete__exact=True) # Todo.objects.filter(complete__exact=True).delete()
+    completed.delete()
+
+    return redirect('index')
+
+def delete_all(request):
+    del_all = Todo.objects.all() #Todo.objects.all().delete()
+    del_all.delete()
+
+    return redirect('index')
+
